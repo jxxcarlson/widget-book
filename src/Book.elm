2b0cabb2ae4204193b6739ddb024cdf8c52d6449
James Carlson <jxxcarlson@gmail.com>
2022-10-12 18:29:20 -0400

    Stateful chapter now working

--- src/Book.elm
+++ src/Book.elm
@@ -2,7 +2,8 @@ module Book exposing (main)
 
 
 import ElmBook exposing (withChapters)
-import FirstChapter exposing (firstChapter)
+import ElmBook.StatefulOptions
+import FirstChapter exposing (chapter_)
 import Element exposing (Element, el, text)
 import Element.Background as Background
 import Element.Events as Events
@@ -17,18 +18,29 @@ import UILibrary.Color as Color
 import ElmBook.ElmUI exposing (Book, book, Chapter)
 import ElmBook.Chapter exposing (chapter, renderComponent)
 
+type alias SharedState =
+    { chapter1Model : FirstChapter.Model
+    }
 
-firstChapter : Chapter x
-firstChapter =
-    chapter "First Chapter"
-        |> renderComponent
-            (el [] <| text "Hello")
+-- firstChapter : Chapter x
+-- firstChapter =
+--     chapter "First Chapter"
+--         |> renderComponent
+--             (el [] <| text "Hello")
+
+initialState : SharedState
+initialState =
+    { chapter1Model = FirstChapter.init
+    }
 
 
-main : Book ()
+main : Book SharedState
 main =
     book "ElmBook with Elm-UI"
+        |> ElmBook.withStatefulOptions
+            [ ElmBook.StatefulOptions.initialState initialState
+            ]
         |> withChapters
-            [ firstChapter
+            [ FirstChapter.chapter_
             ]
 
--- src/FirstChapter.elm
+++ src/FirstChapter.elm
@@ -1,7 +1,9 @@
-module FirstChapter exposing (firstChapter)
+module FirstChapter exposing (Model, init, chapter_)
 
 
-import ElmBook.Chapter exposing (chapter, renderComponent)
+import ElmBook.Chapter exposing (chapter, renderStatefulComponent)
+-- import ElmBook.Chapter exposing (Chapter, chapter, renderStatefulComponent)
+import ElmBook.Actions exposing (mapUpdate)
 import Html exposing (..)
 import Element exposing(Element)
 import UILibrary.Button as Button
@@ -11,12 +13,50 @@ import ElmBook.ElmUI exposing (Book, book, Chapter)
 
 type Msg = NoOp
 
+type alias Model = Int
 
-firstChapter : Chapter x
-firstChapter =
-    chapter "The First Chapter"
-        |> renderComponent
-            (Element.el [] <| Element.text "Hello")
+
+init : Model
+init = 0
+
+
+chapter_ : Chapter { x | chapter1Model : Model }
+chapter_ =
+    chapter "Chapter 1"
+        |> renderStatefulComponent (
+            \{ chapter1Model } ->
+                view chapter1Model
+                    |> Element.map (
+                        mapUpdate
+                            { toState = \state chapter1Model_ -> { state | chapter1Model = chapter1Model_ }
+                            , fromState = \state -> state.chapter1Model
+                            , update = update
+                            }
+                    )
+        )
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        NoOp -> model
+
+
+view : Model -> Element Msg
+view model =
+    -- input [ value model.value, onInput UpdateValue ] []
+   Element.column [] 
+     [ Element.text "This is a test."
+      , testButton
+     ]
+
+
+
+-- firstChapter : Chapter x
+-- firstChapter =
+--     chapter "The First Chapter"
+--         |> renderComponent
+--             (Element.el [] <| Element.text "Hello")
 
 
 component : Element Msg
@@ -24,7 +64,10 @@ component =
    testButton 
 
 
+testButton = Button.template defaultButtonStyle {msg = NoOp, label = "Cancel", tooltipText = "Are you sure?"}
 
+
+-- DEFINITIONS
 
 defaultButtonStyle : Button.Style msg
 defaultButtonStyle = 
@@ -33,7 +76,7 @@ defaultButtonStyle =
    , labelAttributes = [Color.fgGray 1.0, Element.centerX, Element.centerY, Font.size 14]
    }
 
-testButton = Button.template defaultButtonStyle {msg = NoOp, label = "Cancel", tooltipText = "Are you sure?"}
+
 
 
 content : String
